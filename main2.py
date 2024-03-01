bcf28666704a146d2ce638a51c3d6aba116de12f
paramsureliya <148708744+paramsureliya@users.noreply.github.com>
2024-03-01 19:32:50 +0100

    Add files via upload

--- main2.py
+++ main2.py
@@ -11,7 +11,7 @@ synthesiser = pipeline("text-to-speech", "microsoft/speecht5_tts")
 embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
 speaker_embedding = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0)
 
-speech = synthesiser("deep is chutiyo", forward_params={"speaker_embeddings": speaker_embedding})
+speech = synthesiser("hello how are you doing", forward_params={"speaker_embeddings": speaker_embedding})
 
 # Specify the manual file path for the WAV file
 manual_wav_file_path = r"C:\Users\PARAM M. SURELIYA\PycharmProjects\text_to_speech\manual_audio1.wav"
